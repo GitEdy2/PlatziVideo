caaf6f4dae63d26f47251a786eac9624194e0066
Edison Proano <clanuzumaki22@gmail.com>
2020-06-08 23:56:10 -0500

    Creacion de componente de Header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Header = () => {(
     <header className="header">
-        <img className="header__img" src="../images/logo-platzi-video-BW2.png" alt="" />
+        <img className="header__img" src="../assets/logo-platzi-video-BW2.png" alt="" />
 
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="../images/user-icon.png" alt="User" />
+                <img src="../assets/user-icon.png" alt="User" />
                 <p>Perfil</p>
                 <ul>
                     <li><a href="#">Cuenta</a></li>
