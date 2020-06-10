7a87d579576c22e403627df6648b139202760ddc
Edison Proano <clanuzumaki22@gmail.com>
2020-06-10 16:23:47 -0500

    importacion de imagenes a CarouselItems con react

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import '../assets/styles/components/CarouselItem.scss';
+import playIcon from '../assets/static/play-icon.png';
+import plusIcon from '../assets/static/plus-icon.png';
 
 const CarouselItem = () => (
     <div className="carousel-item">
         <img className="carousel-item__img" src="https://images.pexels.com/photos/109919/pexels-photo-109919.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="People" />
             <div className="carousel-item__details">
                 <div>
-                    <img className="carousel-item__details_icons" src="images/play-icon.png" alt="play"/>
-                    <img className="carousel-item__details_icons" src="images/plus-icon.png" alt="plus"/>
+                    <img className="carousel-item__details_icons" src={playIcon} alt="play"/>
+                    <img className="carousel-item__details_icons" src={plusIcon} alt="plus"/>
                 </div>                  
             </div>
         <p className="carousel-item__details--title">Título descriptivo</p>
