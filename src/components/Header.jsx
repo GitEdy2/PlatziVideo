0b8e351de4baeefd3b3e74f4c692cae24393c9ed
Edison Proano <clanuzumaki22@gmail.com>
2020-06-10 16:13:08 -0500

    importacion de imagenes con react para Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import '../assets/styles/components/Header.scss';
+import logo from  '../assets/static/logo-platzi-video-BW2.png';
+import userIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
     <header className="header">
-        <img className="header__img" src="src\assets\logo-platzi-video-BW2.png" alt="" />
+        <img className="header__img" src={logo} alt="" />
 
         <div className="header__menu">
             <div className="header__menu--profile">
-                <img src="src\assets\user-icon.png" alt="User" />
+                <img src={userIcon} alt="User" />
                 <p>Perfil</p>
                 <ul>
                     <li><a href="#">Cuenta</a></li>
