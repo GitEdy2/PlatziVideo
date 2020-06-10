164f0e129ef50a2759648ff3e76807cb899ec5bb
Edison Proano <clanuzumaki22@gmail.com>
2020-06-09 22:10:17 -0500

    Creacion de componente de CarouselItem.jsx

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const CarouselItem = () => (
-    <div class="carousel-item">
-        <img class="carousel-item__img" src="https://images.pexels.com/photos/109919/pexels-photo-109919.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="People" />
-            <div class="carousel-item__details">
+    <div className="carousel-item">
+        <img className="carousel-item__img" src="https://images.pexels.com/photos/109919/pexels-photo-109919.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="People" />
+            <div className="carousel-item__details">
                 <div>
-                    <img class="carousel-item__details_icons" src="images/play-icon.png" alt="play"/>
-                    <img class="carousel-item__details_icons" src="images/plus-icon.png" alt="plus"/>
+                    <img className="carousel-item__details_icons" src="images/play-icon.png" alt="play"/>
+                    <img className="carousel-item__details_icons" src="images/plus-icon.png" alt="plus"/>
                 </div>                  
             </div>
-        <p class="carousel-item__details--title">Título descriptivo</p>
-        <p class="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+        <p className="carousel-item__details--title">Título descriptivo</p>
+        <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
     </div>
 );
 
