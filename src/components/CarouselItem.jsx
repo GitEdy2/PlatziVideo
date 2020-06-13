b44e5700963f95764745115de6fe7945c35637d5
Edison Proano <clanuzumaki22@gmail.com>
2020-06-12 21:36:52 -0500

    Creacion de props para importar informacion de API

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,17 +3,19 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src="https://images.pexels.com/photos/109919/pexels-photo-109919.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" alt="People" />
+        <img className="carousel-item__img" src={cover} alt={title} />
             <div className="carousel-item__details">
                 <div>
                     <img className="carousel-item__details_icons" src={playIcon} alt="play"/>
                     <img className="carousel-item__details_icons" src={plusIcon} alt="plus"/>
                 </div>                  
             </div>
-        <p className="carousel-item__details--title">Título descriptivo</p>
-        <p className="carousel-item__details--subtitle">2019 16+ 114 minutos</p>
+        <p className="carousel-item__details--title">{title}</p>
+        <p className="carousel-item__details--subtitle">
+            {`${year} ${contentRating} ${duration}`}
+        </p>
     </div>
 );
 
