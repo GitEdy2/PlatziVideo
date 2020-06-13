30905e0eb273974e00e4c857f3a9b542d88c5152
Edison Proano <clanuzumaki22@gmail.com>
2020-06-12 23:13:46 -0500

    Implemetacion de prop-types a props de componente.

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import { prototype } from 'file-loader';
 
 const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
@@ -18,5 +20,13 @@ const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
         </p>
     </div>
 );
+
+CarouselItem.propTypes = {
+    cover: PropTypes.string,
+    title: PropTypes.string,
+    year: PropTypes.number,
+    contentRating: PropTypes.string,
+    duration: PropTypes.number,
+};
 
 export default CarouselItem;
