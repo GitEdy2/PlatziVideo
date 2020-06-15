b12db8dd0d45eab1c6923cdeff5cd15f050277c7
Edison Proano <clanuzumaki22@gmail.com>
2020-06-14 22:12:03 -0500

    Configuracion para historial nav de devServer.

--- webpack.config.js
+++ webpack.config.js
@@ -49,6 +49,10 @@ module.exports = {
         ],
     },
 
+    devServer: {
+        historyApiFallback: true,
+    },
+
     plugins: [
         new HtmlWebPackPlugin({
             template: './public/index.html',
