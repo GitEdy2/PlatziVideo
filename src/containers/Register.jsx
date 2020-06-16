faba3bdb115805ff66c261fc8fe2aeb3cc2953af
Edison Proano <clanuzumaki22@gmail.com>
2020-06-15 19:25:46 -0500

    Funcion Link en Register Iniciar Sesion.

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../assets/styles/components/Register.scss';
 
 const Register = () => (
@@ -16,7 +17,9 @@ const Register = () => (
                 </form>
 
                 <div class="register__container--inicio-sesion">
-                    <a href="">Iniciar Sesion</a>
+                    <Link to="/login">
+                        Iniciar Sesion
+                    </Link>
                 </div>
 
             </section>
