38a130906a68473c499008b5859a8b771049bc48
Edison Proano <clanuzumaki22@gmail.com>
2020-06-19 19:37:37 -0500

    implementación de funcion Logout en componente.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from  '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -10,6 +11,10 @@ const Header = (props) => {
 
     const { user } = props;
     const hasUser = Object.keys(user).length > 0;
+
+    const handleLogout = () => {
+        props.logoutRequest({});
+    }
     
     return (
         <header className="header">
@@ -30,12 +35,26 @@ const Header = (props) => {
 
                     <p>Perfil</p>
                     <ul>
-                        <li><a href="/">Cuenta</a></li>
-                        <li>
-                            <Link to="/login">
-                                Iniciar Sesión
-                            </Link>
-                        </li>
+                    
+                    {
+                        hasUser ?
+                            <li><a href="/">{user.name}</a></li>
+                        :
+                        null
+                    }
+
+                    {
+                        hasUser ?
+                            <li><a href="#logout" onClick={handleLogout}>Cerrar Sesión</a></li>
+                            :                      
+                            <li>
+                                <Link to="/login">
+                                    Iniciar Sesión
+                                </Link>
+                            </li>
+                            
+                    }
+                    
                     </ul>
                 </div>
             </div>
@@ -49,5 +68,9 @@ const mapStateToProps = state => {
     };
 };
 
+const mapDispatchToProps = {
+    logoutRequest,
+}
 
-export default connect(mapStateToProps, null)(Header);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
