4cac9102009e5c9aba6d32f7832a72af799654b4
Edison Proano <clanuzumaki22@gmail.com>
2020-06-19 19:37:01 -0500

    Creacion de case Logout dentro de reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -29,6 +29,12 @@ const reducer = (state, action) => {
                 user: action.payload,
             }
 
+        case 'LOGOUT_REQUEST':
+            return {
+                ...state,
+                user: action.payload,
+            }
+
         default:
             return state;
     }
