9e7c1c69e34dae99e27c868b7df0b7e43b47b3c3
Edison Proano <clanuzumaki22@gmail.com>
2020-06-21 16:21:04 -0500

    Implementacion de estilo isHome para Searchbar.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import '../assets/styles/components/Search.scss';
+import classNames from 'classnames';
 
-const Search = () => (
-    <section className="main">
-        <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-        <input className="main__input" type="text" placeholder="Buscar..."/>
-    </section>
-);
+const Search = ({ isHome }) => {
+
+    const inputStyle = classNames('input', {
+        isHome
+    });
+
+    return (
+        <section className="main">
+            <h2 className="main__title">¿Qué quieres ver hoy?</h2>
+            <input className={inputStyle} type="text" placeholder="Buscar..."/>
+        </section>
+    )
+};
 
 export default Search;
