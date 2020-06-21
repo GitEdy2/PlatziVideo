6219b8a49e2edee6c4d6ca676ab4f5a227e8a9f8
Edison Proano <clanuzumaki22@gmail.com>
2020-06-21 15:04:54 -0500

    Implementacion de funciones useState y useEffect.

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-
+import { getVideoSource } from '../actions';
+import { Redirect } from 'react-router-dom';
 import '../assets/styles/components/Player.scss';
 
 const Player = props => {
 
-    const {id} = props.match.params;
+    const { id } = props.match.params;
+    const [ loading, setLoading ] = useState(true);
+    const hasPlaying = Object.keys(props.playing).length > 0;
+    
 
-    return (
+    useEffect(() => {
+        props.getVideoSource(id);
+        setLoading(false);
+    }, []); 
+
+    if (loading) return <h2>Cargando video...</h2>
+
+    return hasPlaying ? (
         <div className="Player">
             <video controls autoPlay>
-                <source src="" type="video/mp4" />
+                <source src={props.playing.source} type="video/mp4" />
             </video>
             <div className="Player-back">
                 <button type="button" onClick={() => props.history.goBack()}>
@@ -18,7 +29,19 @@ const Player = props => {
                 </button>
             </div>
         </div>
-    );
+    )
+    :
+    <Redirect to='/404' />
 };
 
-export default connect(null, null)(Player);
+const mapStateToProps = state => {
+    return {
+        playing: state.playing,
+    }
+};
+
+const mapDispatchToProps = {
+    getVideoSource,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
