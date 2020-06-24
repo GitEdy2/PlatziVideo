715853d8f4681785453e0d91890f728a34f94551
Edison Proano <clanuzumaki22@gmail.com>
2020-06-24 15:49:37 -0500

    Implementacion de props en componente Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,19 +1,56 @@
 import React from 'react';
-import '../assets/styles/components/Search.scss';
 import classNames from 'classnames';
+import { getVideoSearch } from '../actions';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import Categories from './Categories';
+import Carousel from './Carousel';
+import CarouselItem from './CarouselItem';
+import '../assets/styles/components/Search.scss';
 
-const Search = ({ isHome }) => {
+
+const Search = (props) => {
+
+    const { isHome } = props;
 
     const inputStyle = classNames('input', {
         isHome
     });
 
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            props.getVideoSearch(event.target.value);
+        }
+        
+    }
+
     return (
         <section className="main">
             <h2 className="main__title">¿Qué quieres ver hoy?</h2>
-            <input className={inputStyle} type="text" placeholder="Buscar..."/>
+
+            <input 
+                className={inputStyle} 
+                type="text" 
+                placeholder="Buscar..."
+                onKeyUp={handleEnter}
+            />
+
         </section>
     )
 };
 
-export default Search;
+const mapStateToProps = state => {
+    return {
+        searchResult: state.searchResult,
+    }
+}
+
+const mapDispatchToProps = {
+    getVideoSearch,
+}
+
+
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
